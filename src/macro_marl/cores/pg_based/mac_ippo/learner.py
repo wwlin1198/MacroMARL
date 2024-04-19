64a5ea39d966c19da489c4415cc162bc57a1ab22
wwlin1198 <woweilin9@gmail.com>
2024-04-19 17:00:42 -0400

    s

--- src/macro_marl/cores/pg_based/mac_ippo/learner.py
+++ src/macro_marl/cores/pg_based/mac_ippo/learner.py
@@ -99,7 +99,7 @@ class Learner(object):
             V_value = agent.critic_net(jobs)[0]
 
             # Clipped value function
-            value_clip_margin = eps - 0.0001
+            value_clip_margin = eps
             V_clipped = V_value + (Gt - V_value).clamp(-value_clip_margin, value_clip_margin)
             
             # Calculate the critic loss using both the clipped and original value estimates
@@ -128,7 +128,7 @@ class Learner(object):
             pi_entropy = torch.distributions.Categorical(logits=action_logits).entropy().view(obs.shape[0], 
                                                                                 trace_len, 
                                                                                 1)
-            agent.actor_loss = -torch.mean(exp_valid * torch.min(surr1, surr2))
+            agent.actor_loss = -torch.mean(torch.min(surr1, surr2))
 
             agent.actor_optimizer.zero_grad()
             # agent.actor_loss.backward()
@@ -137,6 +137,7 @@ class Learner(object):
             if self.grad_clip_norm:
                 clip_grad_norm_(agent.actor_net.parameters(), self.grad_clip_norm)
             # agent.actor_optimizer.step()
+        
             total_loss = agent.actor_loss * pi_entropy.mean()
             total_loss.backward()
                 
