b0a97ad73e43778c864b2a2cbf942b7123a34e65
wwlin1198 <wlin05@tufts.edu>
2024-04-19 12:59:12 -0400

    n

--- experiments/BoxPushing/mac_ippo.sh
+++ experiments/BoxPushing/mac_ippo.sh
@@ -15,26 +15,27 @@ trap cleanup SIGINT
 # Box Pushing 8x8
 for ((i=0; i<1; i++))
 do
-    pg_based_main.py --save_dir='ma_ippo_bp8' \
+    pg_based_main.py --save_dir='ma_ippo_bp6' \
                     --alg='MacIPPO' \
                     --env_id='BP-MA-v0' \
                     --n_agent=2 \
                     --env_terminate_step=100 \
                     --big_box_reward=300 \
-                    --ippo_clip_value=0.1 \
+                    --ippo_clip_value=0.2 \
                     --ippo_epochs=4 \
-                    --a_lr=0.0003 \
-                    --c_lr=0.003 \
+                    --a_lr=0.0004 \
+                    --c_lr=0.004 \
                     --train_freq=48 \
                     --n_env=48 \
+                    --c_target_update_freq=64 \
                     --n_step_TD=3 \
                     --grad_clip_norm=0 \
                     --eps_start=1.0 \
                     --eps_end=0.01 \
                     --eps_stable_at=4_000 \
                     --total_epi=40_000 \
-                    --grid_dim 10 10 \
-                    --gamma=0.98 \
+                    --grid_dim 6 6 \
+                    --gamma=0.99 \
                     --eval_policy \
                     --sample_epi \
                     --run_id=$i &
--- experiments/BoxPushing/single_mac_IAICC.sh
+++ experiments/BoxPushing/single_mac_IAICC.sh
@@ -10,27 +10,27 @@ cleanup() {
 # Set the trap to call the cleanup function on SIGINT
 trap cleanup SIGINT
 
-# Box Pushing 10x10
-for ((i=0; i<1; i++))
+# Box Pushing 8x8
+for ((i=0; i<5; i++))
 do
-    pg_based_main.py --save_dir='ma_iaicc_bp6' \
+    pg_based_main.py --save_dir='ma_iaicc_bp8' \
                         --alg='MacIAISC' \
                         --env_id='BP-MA-v0' \
                         --n_agent=2 \
                         --env_terminate_step=100 \
                         --big_box_reward=300 \
-                        --a_lr=0.0003 \
-                        --c_lr=0.003 \
+                        --a_lr=0.0004 \
+                        --c_lr=0.004 \
                         --train_freq=32 \
                         --n_env=32 \
-                        --c_target_update_freq=128 \
+                        --c_target_update_freq=64 \
                         --n_step_TD=0 \
                         --grad_clip_norm=0 \
                         --eps_start=1.0 \
                         --eps_end=0.01 \
                         --eps_stable_at=4_000 \
                         --total_epi=40_000 \
-                        --grid_dim 10 10 \
+                        --grid_dim 8 8 \
                         --gamma=0.98 \
                         --eval_policy \
                         --sample_epi \
