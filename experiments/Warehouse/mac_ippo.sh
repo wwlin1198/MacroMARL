488732ce17f56174cad6c3fa7dd80b194e354ec9
wwlin1198 <woweilin9@gmail.com>
2024-05-23 10:52:40 -0400

    mappo

--- experiments/Warehouse/mac_ippo.sh
+++ experiments/Warehouse/mac_ippo.sh
@@ -10,101 +10,101 @@ cleanup() {
 # Set the trap to call the cleanup function on SIGINT
 trap cleanup SIGINT
 
-# Warehouse-A
-for ((i=0; i<3; i++))
-do
-    pg_based_main.py --save_dir='ma_iaicc_warehouse_A' \
-                    --alg='MacIPPO' \
-                    --run_id=$i \
-                    --env_id='OSD-F-v0' \
-                    --n_agent=3 \
-                    --l_mode=0 \
-                    --env_terminate_step=200 \
-                    --a_lr=0.0004 \
-                    --c_lr=0.0004 \
-                    --train_freq=4 \
-                    --n_env=4 \
-                    --c_target_update_freq=32 \
-                    --n_step_TD=5 \
-                    --grad_clip_norm=0 \
-                    --eps_start=1.0 \
-                    --eps_end=0.05 \
-                    --eps_stable_at=10_000 \
-                    --total_epi=40_000 \
-                    --gamma=1.0 \
-                    --a_rnn_layer_size=32 \
-                    --c_rnn_layer_size=64 \
-                    --h0_speed_ps 27 20 20 20 \
-                    --h1_speed_ps 27 20 20 20 \
-                    --d_pen=-20.0 \
-                    --tb_m_speed=0.8 \
-                    --sample_epi \
-                    --eval_policy &
-done
-# Warehouse-B
-for ((i=0; i<20; i++))
-do
-    pg_based_main.py --save_dir='ma_iaicc_warehouse_B' \
-                    --alg='MacIAICC' \
-                    --run_id=$i \
-                    --env_id='OSD-D-v7' \
-                    --n_agent=3 \
-                    --l_mode=0 \
-                    --env_terminate_step=200 \
-                    --a_lr=0.0004 \
-                    --c_lr=0.0004 \
-                    --train_freq=4 \
-                    --n_env=4 \
-                    --c_target_update_freq=32 \
-                    --n_step_TD=5 \
-                    --grad_clip_norm=0 \
-                    --eps_start=1.0 \
-                    --eps_end=0.05 \
-                    --eps_stable_at=10_000 \
-                    --total_epi=40_000 \
-                    --gamma=1.0 \
-                    --a_rnn_layer_size=32 \
-                    --c_rnn_layer_size=64 \
-                    --h0_speed_ps 18 15 15 15 \
-                    --h1_speed_ps 48 18 15 15 \
-                    --d_pen=-20.0 \
-                    --tb_m_speed=0.8 \
-                    --sample_epi \
-                    --eval_policy &
-done
+# # Warehouse-A
+# for ((i=0; i<3; i++))
+# do
+#     pg_based_main.py --save_dir='ma_iaicc_warehouse_A' \
+#                     --alg='MacIPPO' \
+#                     --run_id=$i \
+#                     --env_id='OSD-F-v0' \
+#                     --n_agent=3 \
+#                     --l_mode=0 \
+#                     --env_terminate_step=200 \
+#                     --a_lr=0.0004 \
+#                     --c_lr=0.0004 \
+#                     --train_freq=4 \
+#                     --n_env=4 \
+#                     --c_target_update_freq=32 \
+#                     --n_step_TD=5 \
+#                     --grad_clip_norm=0 \
+#                     --eps_start=1.0 \
+#                     --eps_end=0.05 \
+#                     --eps_stable_at=10_000 \
+#                     --total_epi=40_000 \
+#                     --gamma=1.0 \
+#                     --a_rnn_layer_size=32 \
+#                     --c_rnn_layer_size=64 \
+#                     --h0_speed_ps 27 20 20 20 \
+#                     --h1_speed_ps 27 20 20 20 \
+#                     --d_pen=-20.0 \
+#                     --tb_m_speed=0.8 \
+#                     --sample_epi \
+#                     --eval_policy &
+# done
+# # Warehouse-B
+# for ((i=0; i<4; i++))
+# do
+#     pg_based_main.py --save_dir='ma_iaicc_warehouse_B' \
+#                     --alg='MacIAICC' \
+#                     --run_id=$i \
+#                     --env_id='OSD-D-v7' \
+#                     --n_agent=3 \
+#                     --l_mode=0 \
+#                     --env_terminate_step=200 \
+#                     --a_lr=0.0004 \
+#                     --c_lr=0.0004 \
+#                     --train_freq=4 \
+#                     --n_env=4 \
+#                     --c_target_update_freq=32 \
+#                     --n_step_TD=5 \
+#                     --grad_clip_norm=0 \
+#                     --eps_start=1.0 \
+#                     --eps_end=0.05 \
+#                     --eps_stable_at=10_000 \
+#                     --total_epi=40_000 \
+#                     --gamma=1.0 \
+#                     --a_rnn_layer_size=32 \
+#                     --c_rnn_layer_size=64 \
+#                     --h0_speed_ps 18 15 15 15 \
+#                     --h1_speed_ps 48 18 15 15 \
+#                     --d_pen=-20.0 \
+#                     --tb_m_speed=0.8 \
+#                     --sample_epi \
+#                     --eval_policy &
+# done
 
-# Warehouse-C
-for ((i=0; i<20; i++))
-do
-    pg_based_main.py --save_dir='ma_iaicc_warehouse_C' \
-                    --alg='MacIAICC' \
-                    --run_id=$i \
-                    --env_id='OSD-T-v1' \
-                    --n_agent=4 \
-                    --l_mode=0 \
-                    --env_terminate_step=250 \
-                    --a_lr=0.0004 \
-                    --c_lr=0.0004 \
-                    --train_freq=4 \
-                    --n_env=4 \
-                    --c_target_update_freq=64 \
-                    --n_step_TD=5 \
-                    --grad_clip_norm=0 \
-                    --eps_start=1.0 \
-                    --eps_end=0.01 \
-                    --eps_stable_at=10_000 \
-                    --total_epi=80_000 \
-                    --gamma=1.0 \
-                    --a_rnn_layer_size=32 \
-                    --c_rnn_layer_size=64 \
-                    --h0_speed_ps 40 40 40 40 \
-                    --h1_speed_ps 40 40 40 40 \
-                    --h2_speed_ps 40 40 40 40 \
-                    --d_pen=-20.0 \
-                    --tb_m_speed=0.8 \
-                    --sample_epi \
-                    --eval_policy &
-done
+# # Warehouse-C
+# for ((i=0; i<4; i++))
+# do
+#     pg_based_main.py --save_dir='ma_iaicc_warehouse_C' \
+#                     --alg='MacIPPO' \
+#                     --run_id=$i \
+#                     --env_id='OSD-T-v1' \
+#                     --n_agent=4 \
+#                     --l_mode=0 \
+#                     --env_terminate_step=250 \
+#                     --a_lr=0.0004 \
+#                     --c_lr=0.0004 \
+#                     --train_freq=4 \
+#                     --n_env=4 \
+#                     --c_target_update_freq=64 \
+#                     --n_step_TD=5 \
+#                     --grad_clip_norm=0 \
+#                     --eps_start=1.0 \
+#                     --eps_end=0.01 \
+#                     --eps_stable_at=10_000 \
+#                     --total_epi=80_000 \
+#                     --gamma=1.0 \
+#                     --a_rnn_layer_size=32 \
+#                     --c_rnn_layer_size=64 \
+#                     --h0_speed_ps 40 40 40 40 \
+#                     --h1_speed_ps 40 40 40 40 \
+#                     --h2_speed_ps 40 40 40 40 \
+#                     --d_pen=-20.0 \
+#                     --tb_m_speed=0.8 \
+#                     --sample_epi \
+#                     --eval_policy &
+# done
 
 # Warehouse-D
 for ((i=0; i<4; i++))
@@ -140,39 +140,39 @@ do
 done
 
 
-# Warehouse-E
-for ((i=0; i<3; i++))
-do
-    pg_based_main.py --save_dir='ma_iaicc_warehouse_E' \
-                    --alg='MacIPPO' \
-                    --run_id=$i \
-                    --env_id='OSD-F-v0' \
-                    --n_agent=4 \
-                    --l_mode=0 \
-                    --env_terminate_step=300 \
-                    --a_lr=0.0004 \
-                    --c_lr=0.0004 \
-                    --train_freq=8 \
-                    --n_env=8 \
-                    --c_target_update_freq=64 \
-                    --n_step_TD=5 \
-                    --grad_clip_norm=0 \
-                    --eps_start=1.0 \
-                    --eps_end=0.01 \
-                    --eps_stable_at=10_000 \
-                    --total_epi=100_000 \
-                    --gamma=1.0 \
-                    --a_rnn_layer_size=32 \
-                    --c_rnn_layer_size=64 \
-                    --h0_speed_ps 40 40 40 40 \
-                    --h1_speed_ps 40 40 40 40 \
-                    --h2_speed_ps 40 40 40 40 \
-                    --h3_speed_ps 40 40 40 40 \
-                    --d_pen=-20.0 \
-                    --tb_m_speed=0.8 \
-                    --sample_epi \
-                    --eval_policy &
-done
+# # Warehouse-E
+# for ((i=0; i<3; i++))
+# do
+#     pg_based_main.py --save_dir='ma_iaicc_warehouse_E' \
+#                     --alg='MacIPPO' \
+#                     --run_id=$i \
+#                     --env_id='OSD-F-v0' \
+#                     --n_agent=4 \
+#                     --l_mode=0 \
+#                     --env_terminate_step=300 \
+#                     --a_lr=0.0004 \
+#                     --c_lr=0.0004 \
+#                     --train_freq=8 \
+#                     --n_env=8 \
+#                     --c_target_update_freq=64 \
+#                     --n_step_TD=5 \
+#                     --grad_clip_norm=0 \
+#                     --eps_start=1.0 \
+#                     --eps_end=0.01 \
+#                     --eps_stable_at=10_000 \
+#                     --total_epi=100_000 \
+#                     --gamma=1.0 \
+#                     --a_rnn_layer_size=32 \
+#                     --c_rnn_layer_size=64 \
+#                     --h0_speed_ps 40 40 40 40 \
+#                     --h1_speed_ps 40 40 40 40 \
+#                     --h2_speed_ps 40 40 40 40 \
+#                     --h3_speed_ps 40 40 40 40 \
+#                     --d_pen=-20.0 \
+#                     --tb_m_speed=0.8 \
+#                     --sample_epi \
+#                     --eval_policy &
+# done
 
 # # Ablation
 # for ((i=0; i<3; i++))
